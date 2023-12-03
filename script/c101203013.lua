c0a6126a76d68c089f2b0e88933b829de26446c5
Wind2009-Louse <wind.jackey@qq.com>
2023-12-03 10:05:45 +0800

    Fix code list of ガーディアン for プリンシパグ

--- script/c101203013.lua
+++ script/c101203013.lua
@@ -1,7 +1,7 @@
 --暗之守护神
 local s,id,o=GetID()
 function s.initial_effect(c)
-	aux.AddCodeList(c,25955164,62340868,98434877)
+	aux.AddCodeList(c,25955164,62340868,98434877,101203063)
 	c:EnableReviveLimit()
 	--Cannot special summon
 	local e1=Effect.CreateEffect(c)
