6637141a232d0b0668b3044cc2f05aa014508886
fallenstardust <905309599@qq.com>
2023-12-18 21:11:46 +0800

    fix LEDE-JP039\040

--- script/c101204040.lua
+++ script/c101204040.lua
@@ -90,7 +90,7 @@ function c101204040.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.RegisterEffect(e1,tp)
 end
 function c101204040.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function c101204040.spop(e,tp,eg,ep,ev,re,r,rp)
