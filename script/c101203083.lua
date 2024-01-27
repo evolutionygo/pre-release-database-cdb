44ee75abcae0f1c7ef84285fdb8e6f9885db5092
Wind2009-Louse <wind.jackey@qq.com>
2024-01-27 16:33:36 +0800

    Fix Emergency Apport
    remove CATEGORY_FUSION_SUMMON

--- script/c101203083.lua
+++ script/c101203083.lua
@@ -2,7 +2,7 @@
 local s,id,o=GetID()
 function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
