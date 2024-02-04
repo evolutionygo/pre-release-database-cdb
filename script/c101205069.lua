2f6af33e7256e9c9c3f2f2ad60ece5e5a1e6eb83
Wind2009-Louse <wind.jackey@qq.com>
2024-02-04 15:23:29 +0800

    Fix INFO
    * 黑色魔术师的特殊召唤category
    * 怒之业火的②效果添加1回合1次
    * 黑魔术师防护罩添加卡名记述

--- script/c101205069.lua
+++ script/c101205069.lua
@@ -1,6 +1,7 @@
 --黒魔術のバリア－ミラーフォース－
 local s,id,o=GetID()
 function s.initial_effect(c)
+	aux.AddCodeList(c,101204051,46986414)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
