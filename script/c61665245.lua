1899c546f5c8509856a293e5e4ac5a6b92fb8b9d
zengshangxing <zengsxing@qq.com>
2024-03-25 05:10:38 +0000

    Replace サモン・ソーサレス

--- script/c61665245.lua
+++ script/c61665245.lua
@@ -54,10 +54,10 @@ function c61665245.spop(e,tp,eg,ep,ev,re,r,rp)
 		dc:RegisterEffect(e2)
 		Duel.SpecialSummonComplete()
 	end
-	if tc and Duel.SpecialSummonStep(tc,0,tp,1-tp,false,false,POS_FACEUP_DEFENSE,zone) and tc:IsLocation(LOCATION_MZONE) then
+	if tc and Duel.SpecialSummonStep(tc,0,tp,1-tp,false,false,POS_FACEUP_DEFENSE,zone) then
 		makeDisable(tc)
 		local g=Duel.GetMatchingGroup(c61665245.spfilter2,tp,LOCATION_DECK,0,nil,e,tp,tc:GetRace())
-		if #g>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SelectYesNo(tp,aux.Stringid(61665245,1)) then
+		if tc:IsLocation(LOCATION_MZONE) and #g>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SelectYesNo(tp,aux.Stringid(61665245,1)) then
 			Duel.BreakEffect()
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 			local sc=g:Select(tp,1,1,nil):GetFirst()
