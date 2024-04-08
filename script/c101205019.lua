87c16f9716f713c21769005b058fea405c820e5c
Wind2009-Louse <wind.jackey@qq.com>
2024-04-09 07:21:47 +0800

    Fix 幻禄の天盃龍

--- script/c101205019.lua
+++ script/c101205019.lua
@@ -76,6 +76,7 @@ function s.spop2(e,tp,eg,ep,ev,re,r,rp)
 			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		end
 	end
+	local c=e:GetHandler()
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
