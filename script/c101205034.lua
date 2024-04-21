299fc10625de1b86dfd58650e971d6e37171090b
Wind2009-Louse <wind.jackey@qq.com>
2024-04-21 22:17:01 +0800

    Fix 光と闇の竜王

--- script/c101205034.lua
+++ script/c101205034.lua
@@ -92,7 +92,6 @@ end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
-		Duel.BreakEffect()
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
