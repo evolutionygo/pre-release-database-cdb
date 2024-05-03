fa3269ebb4ca18daeeedd2fd9be031c79d50ef34
Wind2009-Louse <wind.jackey@qq.com>
2024-05-03 12:53:25 +0800

    Fix 五月豹

--- script/c101205031.lua
+++ script/c101205031.lua
@@ -75,7 +75,7 @@ function s.atkcon2(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.atkop2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and c:IsFaceup() and not c:IsAttack(0) then
+	if not c:IsStatus(STATUS_BATTLE_DESTROYED) and c:IsRelateToEffect(e) and c:IsFaceup() and not c:IsAttack(0) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
