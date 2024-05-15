4ad1f57bf17a5ce1b61d8abbbcd7cc58d7d348e5
Wind2009-Louse <wind.jackey@qq.com>
2024-05-15 14:13:53 +0800

    Fix ヴァーチュ・ストリーム

--- script/c101206208.lua
+++ script/c101206208.lua
@@ -61,12 +61,13 @@ function s.attop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
 	else
-		local e1=Effect.CreateEffect(c)
+		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetValue(ATTRIBUTE_WATER)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		c:RegisterEffect(e1)
+		tc:RegisterEffect(e1)
 	end
 end
 function s.valcon(e,re,r,rp)
