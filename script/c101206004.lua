4dcc82c812fce9ea103b59ed1914df639811f64e
Wind2009-Louse <wind.jackey@qq.com>
2024-06-09 12:47:16 +0800

    Add cdb

--- script/c101206004.lua
+++ script/c101206004.lua
@@ -21,7 +21,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 	--special summon
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE)
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetCode(EVENT_CUSTOM+id)
