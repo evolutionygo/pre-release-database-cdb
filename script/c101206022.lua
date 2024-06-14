6689440ae30c1dd236196c02157ce7ad0421c7e2
YGO-Amiya <a1194601505@gmail.com>
2024-06-14 13:44:23 +0800

    新卡

---
+++ script/c101206022.lua
@@ -0,0 +1,51 @@
+--ユニオン・パイロット
+local s,id,o=GetID()
+function s.initial_effect(c)
+	aux.EnableUnionAttribute(c,aux.TRUE)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetCategory(CATEGORY_EQUIP+CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(s.recost)
+	e2:SetTarget(s.retg)
+	e2:SetOperation(s.reop)
+	c:RegisterEffect(e2)
+end
+s.has_text_type=TYPE_UNION
+function s.recost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=e:GetHandler():GetEquipTarget()
+	e:SetLabelObject(tc)
+	if chk==0 then return e:GetHandler():IsAbleToHandAsCost() end
+	Duel.SendtoHand(e:GetHandler(),nil,REASON_COST)
+end
+function s.tgfilter(c,e,tp)
+	return c:IsLocation(LOCATION_MZONE) and c:IsFaceup() and c:IsControler(tp)
+		and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_REMOVED,0,1,nil,c,tp)
+end
+function s.cfilter(c,ec,tp)
+	return c:IsFaceup() and c:IsType(TYPE_UNION)
+		and c:CheckUniqueOnField(tp) and not c:IsForbidden() and c:CheckUnionTarget(ec) and aux.CheckUnionEquip(c,ec)
+end
+function s.retg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0,TYPES_EFFECT_MONSTER,2100,1000,4,RACE_MACHINE,ATTRIBUTE_LIGHT) and Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+	Duel.SetTargetCard(e:GetHandler())
+end
+function s.reop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
+	local tc=e:GetHandler()
+	if tc then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+		local sg=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_REMOVED,0,1,1,nil,tc,tp)
+		local ec=sg:GetFirst()
+		if ec and Duel.Equip(tp,ec,tc) then
+			aux.SetUnionState(ec)
+			if e:GetHandler():IsRelateToChain() and e:GetHandler():IsLocation(LOCATION_HAND) and 
+				Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
+			end
+		end
+	end
+end
