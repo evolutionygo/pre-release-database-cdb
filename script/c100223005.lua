53bb3be8c1b28d508a6a287bd362840cb297e60c
Wind2009-Louse <wind.jackey@qq.com>
2024-07-10 22:42:38 +0800

    Fix

--- script/c100223005.lua
+++ script/c100223005.lua
@@ -2,6 +2,12 @@
 local s,id,o=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
+	--special summon condition
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	c:RegisterEffect(e0)
 	--special summon rule
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
