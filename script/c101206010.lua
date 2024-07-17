055c105714dabc35567084f596a8a55e04593d3f
YGO-Amiya <a1194601505@gmail.com>
2024-07-17 21:15:20 +0800

    修复

--- script/c101206010.lua
+++ script/c101206010.lua
@@ -50,7 +50,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e5)
 end
 function s.tgfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0xc6) and c:IsType(TYPE_PENDULUM) and c:IsAbleToGraveAsCost()
+	return c:IsFaceup() and c:IsSetCard(0xc6) and c:IsType(TYPE_PENDULUM) and c:IsAbleToGrave()
 		and c:GetLeftScale()>0
 end
 function s.ptg(e,tp,eg,ep,ev,re,r,rp,chk)
