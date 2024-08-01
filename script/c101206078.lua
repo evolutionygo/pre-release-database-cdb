aaa82022f3007af823bbac968ce97cbb9bc87fa8
Wind2009-Louse <wind.jackey@qq.com>
2024-08-01 11:23:51 +0800

    Fix ヴァルモニカ・エレディターレ

--- script/c101206078.lua
+++ script/c101206078.lua
@@ -60,13 +60,16 @@ function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 	if ct==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 	local tg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.tdfilter),tp,LOCATION_GRAVE+LOCATION_EXTRA+LOCATION_REMOVED,0,1,ct,nil)
-	if tg:GetCount()>0 and Duel.SendtoDeck(tg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)~=0 then
-		local og=Duel.GetOperatedGroup()
-		local dr=og:FilterCount(Card.IsLocation,nil,LOCATION_DECK+LOCATION_EXTRA)
-		local drc=math.floor(dr/3)
-		if drc>0 and Duel.IsPlayerCanDraw(tp,drc) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-			Duel.BreakEffect()
-			Duel.Draw(tp,drc,REASON_EFFECT)
+	if tg:GetCount()>0 then
+		Duel.HintSelection(tg)
+		if Duel.SendtoDeck(tg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)~=0 then
+			local og=Duel.GetOperatedGroup()
+			local dr=og:FilterCount(Card.IsLocation,nil,LOCATION_DECK+LOCATION_EXTRA)
+			local drc=math.floor(dr/3)
+			if drc>0 and Duel.IsPlayerCanDraw(tp,drc) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+				Duel.BreakEffect()
+				Duel.Draw(tp,drc,REASON_EFFECT)
+			end
 		end
 	end
 end
