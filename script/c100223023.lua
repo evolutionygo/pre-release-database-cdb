b98190238b1ce326eb712662cc16df78b8d1180d
Wind2009-Louse <wind.jackey@qq.com>
2024-08-09 20:42:38 +0800

    Fix

--- script/c100223023.lua
+++ script/c100223023.lua
@@ -28,7 +28,8 @@ end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	local res=e:GetHandler():IsHasEffect(EFFECT_TRAP_ACT_IN_SET_TURN,tp)
-	if chk==0 then return res and res:GetHandler()==c or Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp,nil) end
+	if chk==0 then return res and res:GetHandler()==c
+		or Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE+LOCATION_REMOVED)
 end
 function s.lpfilter(c)
