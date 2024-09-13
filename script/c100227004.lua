2a934c1865bfee12ac22183ee3dc6d9e9765e395
zengsxing <127161407+zengsxing@users.noreply.github.com>
2024-09-13 14:50:29 +0800

    Update c100227004.lua

--- script/c100227004.lua
+++ script/c100227004.lua
@@ -12,7 +12,7 @@ function s.initial_effect(c)
 	--chain limit
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCondition(s.limcon)
 	e2:SetOperation(s.limop)
