ed2622fcf5c8b16ca130e42c241d6a25f55be386
Wind2009-Louse <wind.jackey@qq.com>
2024-09-16 20:09:23 +0800

    Add 封印の魔道士 スプーン

--- script/c100228001.lua
+++ script/c100228001.lua
@@ -74,6 +74,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
 		local tc=g:GetFirst()
 		if tc then
+			Duel.HintSelection(g)
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_SET_ATTACK_FINAL)
