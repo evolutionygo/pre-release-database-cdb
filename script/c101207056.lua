c87f4f4a087f8bd91d0b2115885fb23a514e2480
Wind2009-Louse <wind.jackey@qq.com>
2024-10-10 14:16:03 +0800

    Update setcode
    !setname 0x19b 迪亚贝尔	ディアベル
    !setname 0x119b 迪亚贝尔斯塔尔	ディアベルスター

--- script/c101207056.lua
+++ script/c101207056.lua
@@ -59,7 +59,7 @@ function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 function s.spfilter(c,e,tp)
-	return c:IsSetCard(0x2c5) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0x19b) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
