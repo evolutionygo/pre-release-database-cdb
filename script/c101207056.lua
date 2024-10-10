26aa497cb791f2e3288c07e315a499d7b7b8c0a7
YGO-Amiya <a1194601505@gmail.com>
2024-10-10 13:05:11 +0800

    更新

--- script/c101207056.lua
+++ script/c101207056.lua
@@ -15,6 +15,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--special summon
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
