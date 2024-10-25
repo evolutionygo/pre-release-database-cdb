cabf7c34aef88c2560139cfa6a665cd5acfb090b
Vee4 <mathew.brooksjr@gmail.com>
2024-10-25 04:28:48 +0000

    Update c101207011.lua (summon is not optional)

--- script/c101207011.lua
+++ script/c101207011.lua
@@ -53,7 +53,6 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ConfirmCards(1-tp,g)
 		if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsRelateToEffect(e)
 			and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
-			and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
 			Duel.BreakEffect()
 			Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 		end
