4f603f14dec937a5080f7327a0fa6d3e461ca774
Vee4 <mathew.brooksjr@gmail.com>
2024-10-25 04:16:17 +0000

    Update c101207024.lua

--- script/c101207024.lua
+++ script/c101207024.lua
@@ -10,7 +10,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--spsummon self
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_SPSUMMON_PROC)
 	e2:SetRange(LOCATION_GRAVE+LOCATION_HAND)
