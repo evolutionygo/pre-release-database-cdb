4c00188e5ae06a65e9db99baadcc8b3417a6b9d9
YGO-Amiya <a1194601505@gmail.com>
2024-12-14 16:58:08 +0800

    修复

--- script/c101208009.lua
+++ script/c101208009.lua
@@ -123,7 +123,7 @@ function s.adjustop(e,tp,eg,ep,ev,re,r,rp)
 					function Auxiliary.XyzLevelFreeOperationAlter(f,gf,minc,maxc,alterf,alterdesc,alterop)
 						return  function(e,tp,eg,ep,ev,re,r,rp,c,og,min,max)
 									if og and not min then
-										if og:GetCount()<=maxc and mg:GetCount()>=minc and maxc==minc+2 then
+										if og:GetCount()<maxc and mg:GetCount()>=minc and maxc==minc+2 then
 											local et=maxc-og:GetCount()
 											local exg=og:Filter(Card.IsHasEffect,nil,81096431,tp)
 											local ext=exg:GetClassCount(s.eftfilter,tp)
@@ -154,7 +154,7 @@ function s.adjustop(e,tp,eg,ep,ev,re,r,rp)
 										Duel.Overlay(c,og)
 									else
 										local mg=e:GetLabelObject()
-										if mg:GetCount()<=maxc and mg:GetCount()>=minc and maxc==minc+2 then
+										if mg:GetCount()<maxc and mg:GetCount()>=minc and maxc==minc+2 then
 											local et=maxc-mg:GetCount()
 											local exg=mg:Filter(Card.IsHasEffect,nil,81096431,tp)
 											local ext=exg:GetClassCount(s.eftfilter,tp)
@@ -201,7 +201,7 @@ function s.adjustop(e,tp,eg,ep,ev,re,r,rp)
 					function Auxiliary.XyzLevelFreeOperation(f,gf,minct,maxct)
 						return  function(e,tp,eg,ep,ev,re,r,rp,c,og,min,max)
 									if og and not min then
-										if og:GetCount()<=maxct and og:GetCount()>=minct and maxct==minct+2 then
+										if og:GetCount()<maxct and og:GetCount()>=minct and maxct==minct+2 then
 											local et=maxct-og:GetCount()
 											local exg=og:Filter(Card.IsHasEffect,nil,81096431,tp)
 											local ext=exg:GetClassCount(s.eftfilter,tp)
@@ -232,7 +232,7 @@ function s.adjustop(e,tp,eg,ep,ev,re,r,rp)
 										Duel.Overlay(c,og)
 									else
 										local mg=e:GetLabelObject()
-										if mg:GetCount()<=maxct and mg:GetCount()>=minct and maxct==minct+2 then
+										if mg:GetCount()<maxct and mg:GetCount()>=minct and maxct==minct+2 then
 											local et=maxct-mg:GetCount()
 											local exg=mg:Filter(Card.IsHasEffect,nil,81096431,tp)
 											local ext=exg:GetClassCount(s.eftfilter,tp)
--- script/c81096431.lua
+++ script/c81096431.lua
@@ -91,7 +91,7 @@ function s.adjustop(e,tp,eg,ep,ev,re,r,rp)
 					function Auxiliary.XyzLevelFreeOperationAlter(f,gf,minc,maxc,alterf,alterdesc,alterop)
 						return  function(e,tp,eg,ep,ev,re,r,rp,c,og,min,max)
 									if og and not min then
-										if og:GetCount()<=maxc and mg:GetCount()>=minc and maxc==minc+2 then
+										if og:GetCount()<maxc and mg:GetCount()>=minc and maxc==minc+2 then
 											local et=maxc-og:GetCount()
 											local exg=og:Filter(Card.IsHasEffect,nil,id,tp)
 											local ext=exg:GetClassCount(s.eftfilter,tp)
@@ -122,7 +122,7 @@ function s.adjustop(e,tp,eg,ep,ev,re,r,rp)
 										Duel.Overlay(c,og)
 									else
 										local mg=e:GetLabelObject()
-										if mg:GetCount()<=maxc and mg:GetCount()>=minc and maxc==minc+2 then
+										if mg:GetCount()<maxc and mg:GetCount()>=minc and maxc==minc+2 then
 											local et=maxc-mg:GetCount()
 											local exg=mg:Filter(Card.IsHasEffect,nil,id,tp)
 											local ext=exg:GetClassCount(s.eftfilter,tp)
@@ -169,7 +169,7 @@ function s.adjustop(e,tp,eg,ep,ev,re,r,rp)
 					function Auxiliary.XyzLevelFreeOperation(f,gf,minct,maxct)
 						return  function(e,tp,eg,ep,ev,re,r,rp,c,og,min,max)
 									if og and not min then
-										if mg:GetCount()<=maxct and mg:GetCount()>=minct and maxct==minct+2 then
+										if mg:GetCount()<maxct and mg:GetCount()>=minct and maxct==minct+2 then
 											local et=maxct-mg:GetCount()
 											local exg=mg:Filter(Card.IsHasEffect,nil,id,tp)
 											local ext=exg:GetClassCount(s.eftfilter,tp)
@@ -200,7 +200,7 @@ function s.adjustop(e,tp,eg,ep,ev,re,r,rp)
 										Duel.Overlay(c,og)
 									else
 										local mg=e:GetLabelObject()
-										if mg:GetCount()<=maxct and mg:GetCount()>=minct and maxct==minct+2 then
+										if mg:GetCount()<maxct and mg:GetCount()>=minct and maxct==minct+2 then
 											local et=maxct-mg:GetCount()
 											local exg=mg:Filter(Card.IsHasEffect,nil,id,tp)
 											local ext=exg:GetClassCount(s.eftfilter,tp)
