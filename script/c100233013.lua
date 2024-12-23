14821bd18ac49c385eb9ca82c442bebc70df1937
Wind2009-Louse <wind.jackey@qq.com>
2024-12-23 17:22:39 +0800

    Update 銀河超航行

--- script/c100233013.lua
+++ script/c100233013.lua
@@ -44,6 +44,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
 	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 end
