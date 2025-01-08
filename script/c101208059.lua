83e72c3395facab89f6abafaaa7e60858effa77b
Wind2009-Louse <wind.jackey@qq.com>
2025-01-08 14:44:42 +0800

    Fix cdb

--- script/c101208059.lua
+++ script/c101208059.lua
@@ -3,6 +3,7 @@ local s,id,o=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -40,7 +41,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if tc:IsAbleToHand() and (not tc:IsSetCard(0x2c5) or not tc:IsAbleToGrave() or Duel.SelectOption(tp,1190,1191)==0) then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,tc)
-	else
+	elseif tc:IsAbleToGrave() then
 		Duel.SendtoGrave(tc,REASON_EFFECT)
 	end
 end
