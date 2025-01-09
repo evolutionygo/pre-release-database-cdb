21212e22859e59bac46a46d2e14f6f1a9b4e0b12
Wind2009-Louse <wind.jackey@qq.com>
2025-01-09 22:41:18 +0800

    Fix 罪なき罪宝

--- script/c101208058.lua
+++ script/c101208058.lua
@@ -72,7 +72,7 @@ end
 function s.tnop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsType(TYPE_MONSTER) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_ADD_TYPE)
--- script/c101208073.lua
+++ script/c101208073.lua
@@ -13,28 +13,31 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,id)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 end
 function s.spfilter(c,e,tp)
-	return c:IsFaceupEx() and c:IsSetCard(0x19b) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		and (not c:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) or Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>0)
+	return c:IsFaceupEx() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and (c:IsLocation(LOCATION_SZONE)
+			or c:IsSetCard(0x19b) and Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>0)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_SZONE+LOCATION_REMOVED) and s.spfilter(chkc,e,tp) end
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED+LOCATION_SZONE) and s.spfilter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE+LOCATION_SZONE+LOCATION_REMOVED,0,1,nil,e,tp) end
+		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED+LOCATION_SZONE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE+LOCATION_SZONE+LOCATION_REMOVED,0,1,1,nil,e,tp)
+	local g=aux.SelectTargetFromFieldFirst(tp,s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED+LOCATION_SZONE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
-	if g:GetFirst():IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) then
+	local sc=g:GetFirst()
+	if sc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) then
 		e:SetLabel(1)
 		e:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_HANDES)
 		Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
 		Duel.Hint(HINT_OPSELECTED,1-tp,aux.Stringid(id,0))
-	else
-		e:SetLabel(0)
+	elseif sc:IsLocation(LOCATION_SZONE) then
+		e:SetLabel(2)
 		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
 		Duel.Hint(HINT_OPSELECTED,1-tp,aux.Stringid(id,1))
 	end
