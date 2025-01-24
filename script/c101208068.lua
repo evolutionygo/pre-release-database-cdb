f43e6717d9f260747ab05e222801c81809490d95
Wind2009-Louse <wind.jackey@qq.com>
2025-01-24 10:33:18 +0800

    Fix 夙めてはしろ 二人ではしろ

--- script/c101208068.lua
+++ script/c101208068.lua
@@ -36,13 +36,14 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ct2=edg:GetCount()
 	if ct1+ct2<7 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
-	local sg=edg:Select(1-tp,7-ct1,ct2,nil)
+	local sg=edg:SelectSubGroup(1-tp,aux.TRUE,ct1==7,7-ct1,ct2,nil)
 	local ct=sg:GetCount()
 	if ct>0 then
 		Duel.Remove(sg,POS_FACEDOWN,REASON_RULE,1-tp)
 	end
 	if ct<7 then
 		local rsg=Duel.GetDecktopGroup(1-tp,7-ct)
+		Duel.DisableShuffleCheck()
 		Duel.Remove(rsg,POS_FACEDOWN,REASON_RULE,1-tp)
 	end
 end
