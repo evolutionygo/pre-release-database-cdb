c61017d5c3e5e2ee190450602a35d69a39129876
Wind2009-Louse <wind.jackey@qq.com>
2025-01-29 21:38:19 +0800

    Fix カオティック・エレメンツ

--- script/c101208067.lua
+++ script/c101208067.lua
@@ -35,15 +35,19 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
-	if g:GetCount()>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
+	if g:GetCount()>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 and g:IsExists(Card.IsLocation,1,nil,LOCATION_HAND) then
 		Duel.ConfirmCards(1-tp,g)
+		::cancel::
 		local dg=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,aux.ExceptThisCard(e))
-		if dg:GetCount()>0 and Duel.GetMatchingGroupCount(Card.IsRace,tp,LOCATION_GRAVE,0,nil,RACE_AQUA+RACE_PYRO)>=3 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		if dg:GetCount()>0 and Duel.GetMatchingGroupCount(Card.IsRace,tp,LOCATION_GRAVE,0,nil,RACE_AQUA+RACE_PYRO)>=3
+			and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
 			local tg=dg:CancelableSelect(tp,1,1,nil)
 			if tg then
 				Duel.BreakEffect()
 				Duel.HintSelection(tg)
 				Duel.Destroy(tg,REASON_EFFECT)
+			else
+				goto cancel
 			end
 		end
 	end
