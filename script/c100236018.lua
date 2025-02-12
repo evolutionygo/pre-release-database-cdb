344e5388224663e2ba55286420a3c18a92a26525
Wind2009-Louse <wind.jackey@qq.com>
2025-02-12 20:20:03 +0800

    Fix ロリポー☆ヤミー

--- script/c100236018.lua
+++ script/c100236018.lua
@@ -53,7 +53,7 @@ function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if not tc:IsRelateToEffect(e) then end
+	if not (tc:IsRelateToEffect(e) and aux.NecroValleyFilter()(tc)) then return end
 	local b1=tc:IsAbleToDeck()
 	local b2=e:GetLabel()==1 and tc:IsAbleToRemove()
 	if b1 and (not b2 or not Duel.SelectYesNo(tp,aux.Stringid(id,2))) then
