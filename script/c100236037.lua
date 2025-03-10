23c16ba8dc96a9810be81060f1a2a54733b1f043
YGO-Amiya <a1194601505@gmail.com>
2025-03-10 15:08:11 +0800

    新卡

---
+++ script/c100236037.lua
@@ -0,0 +1,108 @@
+--K9－EX “Werewolf”
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	aux.AddXyzProcedure(c,nil,9,2)
+	c:EnableReviveLimit()
+	--multi attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_EXTRA_ATTACK)
+	e1:SetCondition(s.racon)
+	e1:SetValue(s.raval)
+	c:RegisterEffect(e1)
+	--remove
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCondition(s.rmcon)
+	e1:SetCost(s.rmcost)
+	e1:SetTarget(s.rmtg)
+	e1:SetOperation(s.rmop)
+	c:RegisterEffect(e1)
+end
+function s.racon(e)
+	return e:GetHandler():GetOverlayCount()>1
+end
+function s.raval(e,c)
+	return e:GetHandler():GetOverlayCount()-1
+end
+function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
+	return rp==1-tp
+end
+function s.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		if Duel.GetTurnPlayer()==tp then
+			return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD+LOCATION_GRAVE,1,nil)
+		else
+			return Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)~=0
+				and Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_HAND,1,nil)
+		end
+	end
+	if Duel.GetTurnPlayer()==tp then
+		Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_ONFIELD+LOCATION_GRAVE)
+	else
+		Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_HAND)
+	end
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetTurnPlayer()==tp then
+		local g1=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,nil)
+		local g2=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_GRAVE,nil)
+		local sg=Group.CreateGroup()
+		if g1:GetCount()>0 and (g2:GetCount()==0 or Duel.SelectYesNo(tp,aux.Stringid(id,1))) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+			local sg1=g1:Select(tp,1,1,nil)
+			Duel.HintSelection(sg1)
+			sg:Merge(sg1)
+		end
+		if g2:GetCount()>0 and (sg:GetCount()==0 or Duel.SelectYesNo(tp,aux.Stringid(id,2))) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+			local sg2=g2:Select(tp,1,1,nil)
+			Duel.HintSelection(sg2)
+			sg:Merge(sg2)
+		end
+		Duel.Remove(sg,POS_FACEUP,REASON_EFFECT)
+	else
+		local g0=Duel.GetFieldGroup(tp,0,LOCATION_HAND)
+		Duel.ConfirmCards(tp,g0)
+		local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_HAND,nil)
+		if g:GetCount()>0 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+			local sg=g:Select(tp,1,1,nil)
+			local tc=sg:GetFirst()
+			Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+			tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e1:SetCode(EVENT_PHASE+PHASE_END)
+			e1:SetCountLimit(1)
+			e1:SetReset(RESET_PHASE+PHASE_END)
+			e1:SetLabelObject(tc)
+			e1:SetCondition(s.retcon)
+			e1:SetOperation(s.retop)
+			Duel.RegisterEffect(e1,tp)
+		end
+		Duel.ShuffleHand(1-tp)
+	end
+end
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if tc:GetFlagEffect(id)==0 then
+		e:Reset()
+		return false
+	else
+		return true
+	end
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	Duel.SendtoHand(tc,1-tp,REASON_EFFECT)
+end
