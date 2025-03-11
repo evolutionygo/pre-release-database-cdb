b009e849bb6b14f89b87a65bcc93a35739c36285
八宫一月 <1318052139@qq.com>
2025-03-11 14:22:54 +0000

    add CATEGORY_SEARCH

--- script/c101301061.lua
+++ script/c101301061.lua
@@ -14,7 +14,7 @@ function s.initial_effect(c)
 	--to hand
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_REMOVE)
 	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
