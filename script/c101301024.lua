588344cbb28418f87005ea482a4007501b48ec12
Wind2009-Louse <wind.jackey@qq.com>
2025-03-13 21:22:39 +0800

    Fix 超量妖精ゼータン

--- script/c101301024.lua
+++ script/c101301024.lua
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 	--spsummon from deck
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,id+o)
