9270a8cd9c200e3870a99154df11b8b7c9a5af89
YGO-Amiya <a1194601505@gmail.com>
2025-04-12 22:18:09 +0800

    新卡

---
+++ script/c100240007.lua
@@ -0,0 +1,91 @@
+--
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_RELEASE+CATEGORY_SPECIAL_SUMMON+CATEGORY_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--search
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+end
+function s.cfilter(c,tp)
+	return c:IsRace(RACE_WARRIOR) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsReleasableByEffect()
+		and Duel.GetMZoneCount(tp,c)>0
+end
+function s.spfilter(c,e,tp)
+	return c:IsRace(RACE_WARRIOR) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sumfilter(c)
+	return c:IsRace(RACE_WARRIOR) and c:IsSummonable(true,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local b1=Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil,tp)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp)
+	local b2=Duel.IsExistingMatchingCard(s.sumfilter,tp,LOCATION_HAND,0,1,nil)
+	if chk==0 then return b1 or b2 end
+	local op=0
+	if b1 or b2 then
+		op=aux.SelectFromOptions(tp,
+			{b1,aux.Stringid(id,2),1},
+			{b2,aux.Stringid(id,3),2})
+	end
+	e:SetLabel(op)
+	if op==1 then
+		if e:IsCostChecked() then
+			e:SetCategory(CATEGORY_RELEASE+CATEGORY_SPECIAL_SUMMON)
+		end
+		Duel.SetOperationInfo(0,CATEGORY_RELEASE,nil,1,0,0)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+	else
+		if e:IsCostChecked() then
+			e:SetCategory(CATEGORY_SUMMON)
+		end
+		Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
+	end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==1 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+		local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
+		if g:GetCount()>0 and Duel.Release(g,REASON_EFFECT)>0 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+			if sg:GetCount()>0 then
+				Duel.SpecialSummon(sg,0,tp,tp,true,false,POS_FACEUP)
+			end
+		end
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
+		local tc=Duel.SelectMatchingCard(tp,s.sumfilter,tp,LOCATION_HAND,0,1,1,nil):GetFirst()
+		if tc then
+			Duel.Summon(tp,tc,true,nil)
+		end
+	end
+end
+function s.thfilter(c)
+	return c:IsSetCard(0x93) and c:IsRace(RACE_FAIRY+RACE_WARRIOR) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
