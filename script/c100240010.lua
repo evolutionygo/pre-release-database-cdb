eafb8d05cc7c379a860a1c681cb65d3fbe19b939
YGO-Amiya <a1194601505@gmail.com>
2025-04-17 22:48:30 +0800

    新卡

---
+++ script/c100240010.lua
@@ -0,0 +1,89 @@
+--
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.atkfilter(c)
+	return c:IsSetCard(0x3008) and c:IsType(TYPE_MONSTER) and c:GetAttack()>0
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+		and Duel.IsExistingTarget(s.atkfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,tp)
+	e:SetLabelObject(g:GetFirst())
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g2=Duel.SelectTarget(tp,s.atkfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+end
+function s.filter1(c,e)
+	return c:IsAbleToRemove() and not c:IsImmuneToEffect(e)
+end
+function s.filter2(c,e,tp,m,f,chkf)
+	return c:IsType(TYPE_FUSION) and c:IsSetCard(0x3008) and (not f or f(c))
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
+end
+function s.cfilter(c)
+	return c:IsSetCard(0x3008) and c:IsType(TYPE_NORMAL)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local sc=g:GetFirst()
+	if sc==tc then sc=g:GetNext() end
+	if tc:IsFacedown() or not tc:IsRelateToEffect(e) or not sc:IsRelateToEffect(e) then return end
+	local ac=e:GetLabelObject()
+	if tc==ac then tc=sc end
+	if not ac:IsImmuneToEffect(e) then
+		local atk=tc:GetAttack()
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		ac:RegisterEffect(e1)
+		if not ac:IsHasEffect(EFFECT_REVERSE_UPDATE)
+			and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil) then
+			local chkf=tp
+			local mg1=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_GRAVE,0,nil)
+			local sg1=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
+			local mg2=nil
+			local sg2=nil
+			local ce=Duel.GetChainMaterial(tp)
+			if ce~=nil then
+				local fgroup=ce:GetTarget()
+				mg2=fgroup(ce,e,tp)
+				local mf=ce:GetValue()
+				sg2=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,chkf)
+			end
+			if (sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0)) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+				Duel.BreakEffect()
+				local sg=sg1:Clone()
+				if sg2 then sg:Merge(sg2) end
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+				local tg=sg:Select(tp,1,1,nil)
+				local tc=tg:GetFirst()
+				if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
+					local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
+					tc:SetMaterial(mat1)
+					Duel.Remove(mat1,POS_FACEUP,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+					Duel.BreakEffect()
+					Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+				else
+					local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,nil,chkf)
+					local fop=ce:GetOperation()
+					fop(ce,e,tp,tc,mat2)
+				end
+				tc:CompleteProcedure()
+			end
+		end
+	end
+end
