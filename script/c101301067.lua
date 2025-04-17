4f882cf19ca7a6b52a4a8b77dd5d8b53832127d9
Wind2009-Louse <wind.jackey@qq.com>
2025-04-17 21:08:14 +0800

    Fix Recettes de Nouvellez～ヌーベルズのレシピ帳～

--- script/c101301067.lua
+++ script/c101301067.lua
@@ -27,11 +27,12 @@ function s.initial_effect(c)
 	e3:SetOperation(s.lpop)
 	c:RegisterEffect(e3)
 	--search
+	local custom_code=aux.RegisterMergedDelayedEvent_ToSingleCard(c,id,EVENT_SPSUMMON_SUCCESS)
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,2))
 	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e4:SetCode(EVENT_CUSTOM+id)
+	e4:SetCode(custom_code)
 	e4:SetRange(LOCATION_SZONE)
 	e4:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e4:SetCountLimit(1)
@@ -39,7 +40,6 @@ function s.initial_effect(c)
 	e4:SetTarget(s.thtg)
 	e4:SetOperation(s.thop)
 	c:RegisterEffect(e4)
-	aux.RegisterMergedDelayedEvent(c,id,EVENT_SPSUMMON_SUCCESS)
 end
 function s.cfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0x196) and c:GetOriginalType()&TYPE_MONSTER==TYPE_MONSTER
@@ -51,26 +51,29 @@ function s.target(e,c)
 	return c:IsFaceup()
 end
 function s.cfilter2(c)
-	return c:IsType(TYPE_MONSTER)
+	return c:IsType(TYPE_MONSTER) and c:IsReason(REASON_EFFECT)
 end
 function s.lpcon(e,tp,eg,ep,ev,re,r,rp)
-	return re:GetHandler():GetType()&(TYPE_MONSTER+TYPE_RITUAL)==TYPE_MONSTER+TYPE_RITUAL
+	return re:GetHandler():IsAllTypes(TYPE_RITUAL+TYPE_MONSTER)
 		and eg:IsExists(s.cfilter2,1,nil)
 end
 function s.lpop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLP(1-tp)>=850 then Duel.PayLPCost(1-tp,850) end
+	if Duel.GetLP(1-tp)>=850 then
+		Duel.Hint(HINT_CARD,0,id)
+		Duel.PayLPCost(1-tp,850)
+	end
 end
 function s.cfilter3(c,tp,e)
-	return c:IsLocation(LOCATION_MZONE) and c:IsSummonPlayer(tp) and c:IsCanBeEffectTarget(e) and c:IsType(TYPE_RITUAL) and c:IsLevelAbove(1)
+	return c:IsLocation(LOCATION_MZONE) and c:IsSummonPlayer(tp) and c:IsFaceup()
+		and c:IsCanBeEffectTarget(e) and c:IsType(TYPE_RITUAL) and c:IsLevelAbove(1)
 end
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.cfilter3,1,nil,tp,e)
 end
 function s.thfilter(c)
-	return c:IsSetCard(0x196,0x197) and c:GetType()~=TYPE_SPELL+TYPE_CONTINUOUS and c:IsAbleToHand()
+	return c:IsSetCard(0x196,0x197) and not c:IsAllTypes(TYPE_CONTINUOUS+TYPE_SPELL) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local c=e:GetHandler()
 	local g=eg:Filter(s.cfilter3,nil,tp,e)
 	if chkc then return g:IsContains(chkc) end
 	if chk==0 then return #g>0 and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
@@ -91,7 +94,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 		local tc=Duel.GetFirstTarget()
-		if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		if tc:IsFaceup() and tc:IsRelateToChain() and tc:IsType(TYPE_MONSTER) then
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_LEVEL)
