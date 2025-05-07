154318d9c60944b681e39863db67d634cdc7e832
YGO-Amiya <a1194601505@gmail.com>
2025-05-07 12:25:34 +0800

    新卡

---
+++ script/c100200272.lua
@@ -0,0 +1,46 @@
+--
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_HANDES+CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(Card.IsType,LOCATION_GRAVE,0,1,nil,TYPE_MONSTER)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,200) end
+	Duel.PayLPCost(tp,200)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	if Duel.Draw(p,d,REASON_EFFECT)~=0 then
+		local c=e:GetHandler()
+		local g=Duel.GetOperatedGroup()
+		local tc=g:GetFirst()
+		Duel.ConfirmCards(1-tp,tc)
+		if not tc:IsType(TYPE_MONSTER) then end
+		Duel.BreakEffect()
+		if Duel.IsExistingMatchingCard(Card.IsAttribute,tp,LOCATION_GRAVE,0,1,nil,tc:GetAttribute()) then
+			Duel.ShuffleHand(tp)
+			Duel.DiscardHand(tp,aux.TRUE,1,1,REASON_EFFECT+REASON_DISCARD)
+		else
+			Duel.Draw(tp,1,REASON_EFFECT)
+		end
+		Duel.ShuffleHand(tp)
+	end
+end
