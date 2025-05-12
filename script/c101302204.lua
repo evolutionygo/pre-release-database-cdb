ca05fa64d813c720ad9736537f8e38e486a6a60c
YGO-Amiya <a1194601505@gmail.com>
2025-05-12 17:26:31 +0800

    修复

--- script/c101302204.lua
+++ script/c101302204.lua
@@ -172,7 +172,7 @@ function s.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.penop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
+	if c:IsRelateToEffect(e) and aux.NecroValleyFilter()(tc) then
 		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
 	end
 end
