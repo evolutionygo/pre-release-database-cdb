c7e02da7e89285add9d4106f9ca3c0a2304cc62f
YGO-Amiya <a1194601505@gmail.com>
2025-06-12 13:34:59 +0800

    新卡

---
+++ script/c101302066.lua
@@ -0,0 +1,112 @@
+--メガリス・アナスタシス
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.thcost)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	--draw
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DRAW+CATEGORY_RELEASE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.drcon)
+	e2:SetTarget(s.drtg)
+	e2:SetOperation(s.drop)
+	c:RegisterEffect(e2)
+end
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function s.thfilter(c)
+	return c:IsAbleToHand() and c:IsType(TYPE_MONSTER) and c:IsSetCard(0x138)
+end
+function s.gcheck(g)
+	return g:FilterCount(Card.IsLevelAbove,nil,8)==1
+		and g:FilterCount(Card.IsLevelBelow,nil,4)==1
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+	if #g>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:SelectSubGroup(tp,s.gcheck,false,2,2)
+		if sg then
+			Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		end
+	end
+end
+function s.spfilter(c,tp)
+	return c:IsFaceup() and c:IsType(TYPE_RITUAL) and c:IsSummonType(SUMMON_TYPE_RITUAL) and c:IsSetCard(0x138) and c:IsControler(tp)
+end
+function s.drcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.spfilter,1,nil,tp)
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local b1=Duel.IsPlayerCanDraw(tp,2)
+		and (not e:IsCostChecked() or Duel.GetFlagEffect(tp,id)==0)
+	local b2=Duel.IsExistingMatchingCard(Card.IsReleasableByEffect,tp,0,LOCATION_MZONE,1,nil)
+		and (not e:IsCostChecked() or Duel.GetFlagEffect(tp,id+o)==0)
+	if chk==0 then return b1 or b2 end
+	local op=0
+	if b1 or b2 then
+		op=aux.SelectFromOptions(tp,
+			{b1,aux.Stringid(id,1),1},
+			{b2,aux.Stringid(id,2),2})
+	end
+	e:SetLabel(op)
+	if op==1 then
+		if e:IsCostChecked() then
+			e:SetCategory(CATEGORY_DRAW+CATEGORY_HANDES)
+			Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+		end
+		Duel.SetTargetPlayer(tp)
+		Duel.SetTargetParam(2)
+		Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+		Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
+	elseif op==2 then
+		if e:IsCostChecked() then
+			e:SetCategory(CATEGORY_RELEASE)
+			Duel.RegisterFlagEffect(tp,id+o,RESET_PHASE+PHASE_END,0,1)
+		end
+		Duel.SetOperationInfo(0,CATEGORY_RELEASE,nil,1,1-tp,LOCATION_MZONE)
+	end
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==1 then
+		local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+		if Duel.Draw(p,2,REASON_EFFECT)==2 then
+			Duel.ShuffleHand(p)
+			Duel.BreakEffect()
+			Duel.DiscardHand(p,nil,1,1,REASON_EFFECT+REASON_DISCARD)
+		end
+	elseif e:GetLabel()==2 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+		local g=Duel.SelectMatchingCard(tp,Card.IsReleasableByEffect,tp,0,LOCATION_MZONE,1,1,nil)
+		local tc=g:GetFirst()
+		if tc then
+			Duel.BreakEffect()
+			Duel.Release(tc,REASON_EFFECT)
+		end
+	end
+end
