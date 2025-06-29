2288d2686ee3ac37d447e9f6f4c68698c395f5c1
YGO-Amiya <a1194601505@gmail.com>
2025-06-29 21:08:19 +0800

    新卡

---
+++ script/c101302069.lua
@@ -0,0 +1,108 @@
+--
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_SEARCH+CATEGORY_TOHAND+CATEGORY_DECKDES)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.costfilter(c,e,tp)
+	return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_HAND,0,1,nil,e,tp)
+		and Duel.GetMZoneCount(tp,c)>0
+end
+function s.spfilter(c,e,tp)
+	return c:IsAttribute(ATTRIBUTE_FIRE) and c:IsLevelAbove(6)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local check=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0
+	local b1=Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp,check)
+		and Duel.GetFlagEffect(tp,id+o)==0
+	local b2=Duel.CheckReleaseGroup(tp,s.costfilter,1,nil,e,tp)
+		and Duel.GetFlagEffect(tp,id)==0
+	if chk==0 then return b1 or b2 end
+	local op=0
+	if b1 or b2 then
+		op=aux.SelectFromOptions(tp,
+			{b1,aux.Stringid(id,1),1},
+			{b2,aux.Stringid(id,2),2})
+	end
+	e:SetLabel(op)
+	if op==1 then
+		e:SetLabel(0)
+	else
+		e:SetLabel(1)
+		local g=Duel.SelectReleaseGroup(tp,s.costfilter,1,1,nil,e,tp)
+		Duel.Release(g,REASON_COST)
+	end
+end
+function s.thfilter(c,e,tp,check)
+	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsRace(RACE_DINOSAUR) and
+		(c:IsAbleToHand()
+			or check and c:IsCanBeSpecialSummoned(e,0,tp,false,false))
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local check=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		local b1=Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp,check)
+			and Duel.GetFlagEffect(tp,id+o)==0
+		local b2=Duel.CheckReleaseGroup(tp,s.costfilter,1,nil,e,tp)
+			and Duel.GetFlagEffect(tp,id)==0
+		if e:IsCostChecked() then
+			return b1 or b2
+		else
+			return b1
+		end
+	end
+	if e:IsCostChecked() and e:GetLabel()==1 then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_HAND)
+	elseif e:IsCostChecked() then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_DECKDES)
+	end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==1 then
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+		if g:GetCount()>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	else
+		local check=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+			and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,check)
+		if g:GetCount()<=0 then return end
+		local tc=g:GetFirst()
+		local b=check and tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		if tc:IsAbleToHand() and (not b or Duel.SelectOption(tp,1190,1152)==0) then
+			Duel.SendtoHand(tc,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tc)
+		elseif b then
+			if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)~=0
+				and not tc:IsRace(RACE_FIEND) then
+				local e1=Effect.CreateEffect(e:GetHandler())
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_DISABLE)
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+				tc:RegisterEffect(e1)
+				local e2=Effect.CreateEffect(e:GetHandler())
+				e2:SetType(EFFECT_TYPE_SINGLE)
+				e2:SetCode(EFFECT_DISABLE_EFFECT)
+				e2:SetValue(RESET_TURN_SET)
+				e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+				tc:RegisterEffect(e2)
+			end
+			Duel.SpecialSummonComplete()
+		end
+	end
+end
