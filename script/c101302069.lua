036a1244af98ad353ae68c57243f3bb837556b55
Wind2009-Louse <wind.jackey@qq.com>
2025-06-29 22:36:32 +0800

    Fix ヘル・パニッシャー

--- script/c101302069.lua
+++ script/c101302069.lua
@@ -1,12 +1,12 @@
---
+--ヘル・パニッシャー
 local s,id,o=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_SEARCH+CATEGORY_TOHAND+CATEGORY_DECKDES)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
@@ -19,62 +19,51 @@ function s.spfilter(c,e,tp)
 	return c:IsAttribute(ATTRIBUTE_FIRE) and c:IsLevelAbove(6)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local check=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0
-	local b1=Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp,check)
-		and Duel.GetFlagEffect(tp,id+o)==0
-	local b2=Duel.CheckReleaseGroup(tp,s.costfilter,1,nil,e,tp)
-		and Duel.GetFlagEffect(tp,id)==0
-	if chk==0 then return b1 or b2 end
-	local op=0
-	if b1 or b2 then
-		op=aux.SelectFromOptions(tp,
-			{b1,aux.Stringid(id,1),1},
-			{b2,aux.Stringid(id,2),2})
-	end
-	e:SetLabel(op)
-	if op==1 then
-		e:SetLabel(0)
-	else
-		e:SetLabel(1)
-		local g=Duel.SelectReleaseGroup(tp,s.costfilter,1,1,nil,e,tp)
-		Duel.Release(g,REASON_COST)
-	end
-end
-function s.thfilter(c,e,tp,check)
-	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsRace(RACE_DINOSAUR) and
-		(c:IsAbleToHand()
-			or check and c:IsCanBeSpecialSummoned(e,0,tp,false,false))
+function s.thfilter(c)
+	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsRace(RACE_DINOSAUR) and c:IsAbleToHand()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		local b1=Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
-			and Duel.GetFlagEffect(tp,id+o)==0
-		local b2=Duel.CheckReleaseGroup(tp,s.costfilter,1,nil,e,tp)
-			and Duel.GetFlagEffect(tp,id)==0
-		if e:IsCostChecked() then
-			return b1 or b2
-		else
-			return b1
-		end
-	end
-	if e:GetLabel()==1 then
+	local b1=Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil)
+		and (not e:IsCostChecked() or Duel.GetFlagEffect(tp,id)==0)
+	local b2=not e:IsCostChecked() and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_HAND,0,1,nil,e,tp)
+		or Duel.CheckReleaseGroup(tp,s.costfilter,1,nil,e,tp) and Duel.GetFlagEffect(tp,id+o)==0
+	if chk==0 then return b1 or b2 end
+	local op=aux.SelectFromOptions(tp,
+			{b1,aux.Stringid(id,1),1},
+			{b2,aux.Stringid(id,2),2})
+	e:SetLabel(op)
+	if op==1 then
 		if e:IsCostChecked() then
 			e:SetCategory(CATEGORY_SPECIAL_SUMMON)
 			Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
 		end
 		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_HAND)
-	else
+	elseif op==2 then
 		if e:IsCostChecked() then
-			Duel.RegisterFlagEffect(tp,id+o,RESET_PHASE+PHASE_END,0,1)
+			local g=Duel.SelectReleaseGroup(tp,s.costfilter,1,1,nil,e,tp)
+			Duel.Release(g,REASON_COST)
 			e:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND+CATEGORY_SEARCH)
+			Duel.RegisterFlagEffect(tp,id+o,RESET_PHASE+PHASE_END,0,1)
 		end
 		Duel.SetOperationInfo(0,CATEGORY_SEARCH+CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 	end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetLabel()==1 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if g:GetCount()>0 then
+			local tc=g:GetFirst()
+			Duel.SendtoHand(tc,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tc)
+			if Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+				and tc:IsType(TYPE_MONSTER) and tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+				and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+				Duel.BreakEffect()
+				Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+			end
+		end
+	elseif e:GetLabel()==2 then
 		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_HAND,0,1,1,nil,e,tp)
@@ -94,21 +83,5 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			tc:RegisterEffect(e2)
 		end
 		Duel.SpecialSummonComplete()
-	else
-		local check=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-			and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
-		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-		if g:GetCount()>0 then
-			local tc=g:GetFirst()
-			Duel.SendtoHand(tc,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,tc)
-			if check and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-				and tc:IsType(TYPE_MONSTER) and tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
-				and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
-				Duel.BreakEffect()
-				Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-			end
-		end
 	end
 end
