aaf6a2d6784529eb32e11c91064009341bf529e0
YGO-Amiya <a1194601505@gmail.com>
2025-07-12 21:13:48 +0800

    修复

--- script/c101302032.lua
+++ script/c101302032.lua
@@ -98,7 +98,7 @@ function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
+	if tc:IsRelateToChain() then
 		Duel.SendtoGrave(tc,REASON_EFFECT)
 	end
 end
