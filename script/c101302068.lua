860a54d1eeef8e1613e997095e5bff4b93a1d8f2
YGO-Amiya <a1194601505@gmail.com>
2025-07-19 22:55:31 +0800

    修复

--- script/c101302068.lua
+++ script/c101302068.lua
@@ -67,10 +67,11 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local op,lk=e:GetLabel()
 	if op==1 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-		local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 		if g:GetCount()>0 then
-			Duel.SendtoGrave(g,REASON_EFFECT)
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
 		end
 	elseif op==2 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
