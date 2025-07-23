b5cc3350383e9047f3ddde122155274856072325
YGO-Amiya <a1194601505@gmail.com>
2025-07-23 20:28:14 +0800

    修复

--- script/c101302078.lua
+++ script/c101302078.lua
@@ -31,7 +31,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+	if tc:IsFaceup() and tc:IsRelateToChain() and aux.NecroValleyFilter()(tc) then
 		local atk=tc:GetAttack()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 		local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_EXTRA+LOCATION_DECK,0,1,1,nil,atk)
