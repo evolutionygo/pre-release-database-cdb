b753fc96e39414993f26ee76a8abd9c918de69a3
YGO-Amiya <a1194601505@gmail.com>
2025-07-25 22:21:35 +0800

    修复

--- script/c101302072.lua
+++ script/c101302072.lua
@@ -51,6 +51,7 @@ function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 			{b3,aux.Stringid(id,3),3},
 			{b4,aux.Stringid(id,4),4})
 	Duel.RemoveCounter(tp,1,0,0x70,op,REASON_COST)
+	e:SetLabel(op)
 	if op==1 or op==2 then
 		e:SetCategory(CATEGORY_ATKCHANGE)
 	elseif op==3 then
