853388282404ca065be27973f4a8a1e036aa15ef
YGO-Amiya <a1194601505@gmail.com>
2025-07-25 14:32:55 +0800

    新卡

---
+++ script/c101302027.lua
@@ -0,0 +1,91 @@
+--蹴神-VARefar
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_BE_BATTLE_TARGET)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.atkcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop1)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetCondition(s.tgcon)
+	e2:SetTarget(s.sptg2)
+	e2:SetOperation(s.spop2)
+	c:RegisterEffect(e2)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local at=Duel.GetAttackTarget()
+	return Duel.GetAttacker():IsControler(1-tp) and at:IsControler(tp)
+end
+function s.cfilter(c,tp)
+	return c:IsLocation(LOCATION_MZONE) and c:IsControler(tp)
+end
+function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	if rp~=1-tp or not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	local rc=re:GetHandler()
+	return rc:IsLocation(LOCATION_MZONE) and re:IsActiveType(TYPE_MONSTER) and g and g:IsExists(s.cfilter,1,nil,tp)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.cfilter2(c,ac)
+	return not c:IsPublic()
+		and (c:IsType(TYPE_MONSTER) and ac:IsCanChangePosition() and ac:IsPosition(POS_ATTACK)
+		or c:IsType(TYPE_SPELL) and ac:IsFaceup()
+		or c:IsType(TYPE_TRAP) and ac:IsAbleToRemove())
+end
+function s.spop1(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToChain() and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		local ac=Duel.GetAttacker()
+		if ac and ac:IsRelateToBattle() then
+			s.cfop(e,tp,eg,ep,ev,re,r,rp,ac)
+		end
+	end
+end
+function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetTargetCard(re:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToChain() and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		local ac=Duel.GetFirstTarget()
+		if ac and ac:IsRelateToEffect(e) then
+			s.cfop(e,tp,eg,ep,ev,re,r,rp,ac)
+		end
+	end
+end
+function s.cfop(e,tp,eg,ep,ev,re,r,rp,ac)
+	local g=Duel.GetMatchingGroup(s.cfilter2,tp,LOCATION_HAND,0,nil,ac)
+	if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+		local tc=g:Select(tp,1,1,nil):GetFirst()
+		if tc:IsType(TYPE_MONSTER) then
+			Duel.ChangePosition(ac,POS_FACEUP_DEFENSE)
+		elseif tc:IsType(TYPE_MONSTER) then
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+			e1:SetValue(ac:GetAttack()*2)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			ac:RegisterEffect(e1)
+		elseif tc:IsType(TYPE_TRAP) then
+			Duel.Remove(ac,POS_FACEUP,REASON_EFFECT)
+		end
+	end
+end
---
+++ script/c101302031.lua
@@ -0,0 +1,54 @@
+--陰陽師 タオタオ
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--ind
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(s.indtg)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_GRAVE_SPSUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_DELAY)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+end
+function s.indtg(e,c)
+	local tc=e:GetHandler()
+	return c==tc or c==tc:GetBattleTarget()
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return ep==tp and bit.band(r,REASON_BATTLE+REASON_EFFECT)~=0
+end
+function s.spfilter(c,e,tp)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and (not c:IsLocation(LOCATION_EXTRA) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		or Duel.GetLocationCountFromEx(tp,tp,nil,c)>0)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local loc=LOCATION_HAND+LOCATION_GRAVE
+	if ev>=2000 then loc=loc+LOCATION_DECK+LOCATION_EXTRA end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,loc,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,loc)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp)
+	local loc=LOCATION_HAND+LOCATION_GRAVE
+	if ev>=2000 then loc=loc+LOCATION_DECK+LOCATION_EXTRA end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,loc,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
