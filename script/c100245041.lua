dcc1e8cec0567980419c50aaa08fdd11b0c1f65c
YGO-Amiya <a1194601505@gmail.com>
2025-08-11 14:34:55 +0800

    新卡

---
+++ script/c100245041.lua
@@ -0,0 +1,51 @@
+--キラーチューン・プレイリスト
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(2,id+EFFECT_COUNT_CODE_OATH)
+	e1:SetHintTiming(TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.filter(c)
+	if not (c:IsFaceupEx() and c:IsSetCard(0x2d6) and c:IsAbleToHand()) then return false end
+	local te=c.killer_tune_be_material_effect
+	if not te then return false end
+	local tg=te:GetTarget()
+	return not tg or tg(e,tp,eg,ep,ev,re,r,rp,0,nil,c)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then
+		local te=e:GetLabelObject()
+		local tg=te:GetTarget()
+		return tg and tg(e,tp,eg,ep,ev,re,r,rp,0,chkc)
+	end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,1,1,nil)
+	local tc=g:GetFirst()
+	Duel.ClearTargetCard()
+	local te=tc.killer_tune_be_material_effect
+	e:SetLabelObject(te)
+	local tg=te:GetTarget()
+	if tg then tg(e,tp,eg,ep,ev,re,r,rp,1) end
+	Duel.ClearOperationInfo(0)
+	g:GetFirst():CreateEffectRelation(e)
+	local tg=te:GetTarget()
+	te:SetLabelObject(e:GetLabelObject())
+	e:SetLabelObject(te)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local te=e:GetLabelObject()
+	if not te then return end
+	local tc=te:GetHandler()
+	if not (tc:IsRelateToChain()) then return end
+	e:SetLabelObject(te:GetLabelObject())
+	local op=te:GetOperation()
+	if op then op(e,tp,eg,ep,ev,re,r,rp) end
+	Duel.SendtoHand(tc,nil,REASON_EFFECT)
+end
