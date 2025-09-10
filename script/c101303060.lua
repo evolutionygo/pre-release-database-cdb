8caacf9301e3814f67e7f125f021dd13a586bed7
YGO-Amiya <a1194601505@gmail.com>
2025-09-10 14:46:22 +0800

    修复

--- script/c101303060.lua
+++ script/c101303060.lua
@@ -50,7 +50,7 @@ function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 function s.thfilter(c)
-	return not c:IsCode(id) and c:IsSetCard(0x45) and c:IsAbleToHand()
+	return not c:IsType(TYPE_FIELD) and c:IsSetCard(0x45) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
