0632b24e8994b403c4c6d99cd393f3cae6c14984
Wind2009-Louse <wind.jackey@qq.com>
2025-09-28 21:56:30 +0800

    Fix スレット・アームド・ドラゴン

--- script/c101303028.lua
+++ script/c101303028.lua
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 	--destroy
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON+CATEGORY_DECKDES)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
