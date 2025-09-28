d29608d36be9716dd934c6ea83ca854ee44f52f7
Wind2009-Louse <wind.jackey@qq.com>
2025-09-28 22:10:12 +0800

    Fix スレット・アームド・ドラゴン

--- script/c101303028.lua
+++ script/c101303028.lua
@@ -43,7 +43,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToChain() and tc:IsType(TYPE_MONSTER) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
-		if c:IsRelateToChain() then
+		if c:IsRelateToChain() and aux.NecroValleyFilter()(c) then
 			Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 		end
 	end
