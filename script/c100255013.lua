26738d49f66b58d4efbb3db7fcde00125298e246
YGO-Amiya <a1194601505@gmail.com>
2025-11-17 19:52:39 +0800

    修复

--- script/c100255013.lua
+++ script/c100255013.lua
@@ -67,12 +67,6 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 		tc:RegisterEffect(e1)
 		if not tc:IsHasEffect(EFFECT_REVERSE_UPDATE) then
 			Duel.ChainAttack()
-			local e2=Effect.CreateEffect(c)
-			e2:SetType(EFFECT_TYPE_SINGLE)
-			e2:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
-			e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-			e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
-			ec:RegisterEffect(e2)
 		end
 	end
 end
