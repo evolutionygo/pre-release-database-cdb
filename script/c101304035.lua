01bc334f0a3453f3bc5bb2ba14d46f6026960339
YGO-Amiya <a1194601505@gmail.com>
2026-01-02 20:56:42 +0800

    新卡

--- script/c101304035.lua
+++ script/c101304035.lua
@@ -5,6 +5,7 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--control
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetCategory(CATEGORY_CONTROL)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
