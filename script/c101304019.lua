b9656bf6726a15b735da12a0d9035dc49b41d63d
YGO-Amiya <a1194601505@gmail.com>
2026-01-09 21:40:46 +0800

    新卡

---
+++ script/c101304019.lua
@@ -0,0 +1,268 @@
+--糾罪巧γ'-「exapatisIA」
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--pendulum summon
+	aux.EnablePendulumAttribute(c)
+	c:EnableCounterPermit(0x71,LOCATION_PZONE)
+	--counter
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e0:SetCode(EVENT_FLIP)
+	e0:SetRange(LOCATION_PZONE)
+	e0:SetOperation(s.ctop)
+	c:RegisterEffect(e0)
+	--search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.thcost)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	--spsummon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCost(s.spcost)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+	--search 2
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.thcon2)
+	e3:SetCost(s.thcost2)
+	e3:SetTarget(s.thtg2)
+	e3:SetOperation(s.thop2)
+	c:RegisterEffect(e3)
+	--flip
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,3))
+	e4:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP)
+	e4:SetTarget(s.destg)
+	e4:SetOperation(s.desop)
+	c:RegisterEffect(e4)
+	--set effect
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e5:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e5:SetCode(EVENT_ADJUST)
+	e5:SetRange(0xff)
+	e5:SetOperation(s.adjustop)
+	c:RegisterEffect(e5)
+	if not s.global_check then
+		s.global_check=true
+		local SST_IsSSetable=Card.IsSSetable
+		function Card.IsSSetable(card,bool)
+			Duel.RegisterFlagEffect(0,id,RESET_CHAIN,0,1)
+			if not bool then
+				return SST_IsSSetable(card,false)
+			else
+				return SST_IsSSetable(card,bool)
+			end
+		end
+		local SST_IsCanBeSpecialSummoned=Card.IsCanBeSpecialSummoned
+		function Card.IsCanBeSpecialSummoned(card,e,sum,tp,bool1,bool2,pos,sp,zone)
+			if pos and pos&POS_FACEDOWN==POS_FACEDOWN then
+				Duel.RegisterFlagEffect(0,id,RESET_CHAIN,0,1)
+			end
+			if not zone then
+				if not sp then
+					if not pos then
+						return SST_IsCanBeSpecialSummoned(card,e,sum,tp,bool1,bool2)
+					else
+						return SST_IsCanBeSpecialSummoned(card,e,sum,tp,bool1,bool2,pos)
+					end
+				else
+					return SST_IsCanBeSpecialSummoned(card,e,sum,tp,bool1,bool2,pos,sp)
+				end
+			else
+				return SST_IsCanBeSpecialSummoned(card,e,sum,tp,bool1,bool2,pos,sp,zone)
+			end
+		end
+	end
+	Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,s.counterfilter)
+end
+function s.counterfilter(c)
+	return c:IsFacedown()
+end
+function s.sttg(tg)
+	return function(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+		if chk==0 then
+			return tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+		end
+		if chk~=0 then
+			tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+			Duel.SetOperationInfo(0,0x100000000,eg,1,0,0)
+		end
+	end
+end
+function s.adjustop(e,tp,eg,ep,ev,re,r,rp)
+	if not s.globle_check then
+		s.globle_check=true
+		local g=Duel.GetMatchingGroup(aux.TRUE,0,0xff,0xff,nil)
+		dual_RegisterEffect=Card.RegisterEffect
+		Card.RegisterEffect=function(Card_c,Effect_e,bool)
+			if Effect_e and Effect_e:GetType() then
+				if Effect_e:GetType()&(EFFECT_TYPE_IGNITION+EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_QUICK_F+EFFECT_TYPE_QUICK_O)~=0 then
+					local tg=Effect_e:GetTarget()
+					if tg then
+						tg(e,tp,Group.CreateGroup(),ep,ev,re,r,rp,0)
+						tg(e,1-tp,Group.CreateGroup(),ep,ev,re,r,rp,0)
+						if Duel.GetFlagEffect(0,id)~=0 then
+							Effect_e:SetTarget(s.sttg(tg))
+						end
+						Duel.ResetFlagEffect(0,id)
+					end
+				end
+			end
+			if bool then
+				dual_RegisterEffect(Card_c,Effect_e,bool)
+			else
+				dual_RegisterEffect(Card_c,Effect_e,false)
+			end
+		end
+		for tc in aux.Next(g) do
+			if tc.initial_effect then
+				local dual_initial_effect=s.initial_effect
+				s.initial_effect=function() end
+				tc:ReplaceEffect(id,0)
+				s.initial_effect=dual_initial_effect
+				tc.initial_effect(tc)
+			end
+		end
+		Card.RegisterEffect=dual_RegisterEffect
+	end
+	e:Reset()
+end
+function s.ctop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	c:AddCounter(0x71,1)
+end
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,900) end
+	Duel.PayLPCost(tp,900)
+end
+function s.thfilter(c)
+	return c:IsSetCard(0x1d4) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,3,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+	if g:GetCount()>=3 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,3,3,nil)
+		Duel.ConfirmCards(1-tp,sg)
+		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_ATOHAND)
+		local tg=sg:RandomSelect(1-tp,1)
+		Duel.ShuffleDeck(tp)
+		tg:GetFirst():SetStatus(STATUS_TO_HAND_WITHOUT_CONFIRM,true)
+		Duel.SendtoHand(tg,nil,REASON_EFFECT)
+	end
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not e:GetHandler():IsPublic()
+		and Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_LIMIT_SPECIAL_SUMMON_POSITION)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(s.splimit)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.splimit(e,c,tp,sumtp,sumpos)
+	return (sumpos&POS_FACEUP)>0
+end
+function s.spfilter(c,e,tp)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		--- This is not activatable under 聖なる輝き
+		if Duel.IsPlayerAffectedByEffect(tp,EFFECT_DIVINE_LIGHT) then
+			return false
+		end
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	Duel.ShuffleHand(tp)
+	if g:GetCount()>0 then
+		local sc=g:GetFirst()
+		local hint=sc:IsPublic()
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)
+		if hint then
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
+function s.thcon2(e,tp,eg,ep,ev,re,r,rp)
+	local ex=Duel.GetOperationInfo(ev,0x100000000)
+	return ep~=tp and ex and e:GetHandler():IsFacedown()
+end
+function s.thcost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.ChangePosition(e:GetHandler(),POS_FACEUP_DEFENSE)
+end
+function s.thfilter2(c)
+	return c:IsSetCard(0x1d4) and c:IsAbleToHand()
+end
+function s.thtg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter2,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter2,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return false end
+	local g=Duel.GetMatchingGroup(Card.IsType,tp,0,LOCATION_ONFIELD,nil,TYPE_SPELL+TYPE_TRAP)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,Card.IsType,tp,0,LOCATION_ONFIELD,1,1,nil,TYPE_SPELL+TYPE_TRAP)
+	if g:GetCount()>0 then
+		Duel.HintSelection(g)
+		if Duel.Destroy(g,REASON_EFFECT)~=0 then
+			if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+			Duel.ShuffleHand(tp)
+			if g:GetCount()>0 then
+				local sc=g:GetFirst()
+				local hint=sc:IsPublic()
+				Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)
+				if hint then
+					Duel.ConfirmCards(1-tp,g)
+				end
+			end
+		end
+	end
+end
