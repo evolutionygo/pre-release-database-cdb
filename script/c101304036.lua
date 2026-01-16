ec2a80286793895fb47e918b698031ddf2c10894
zengsxing <127161407+zengsxing@users.noreply.github.com>
2026-01-16 16:21:14 +0800

    update blzd

---
+++ script/c101304036.lua
@@ -0,0 +1,79 @@
+--10星
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--Synchro Summon
+	c:EnableReviveLimit()
+	aux.AddSynchroProcedure(c,nil,aux.NonTuner(Card.IsType,TYPE_SYNCHRO),1)
+	--Increase Level
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_UPDATE_LEVEL)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(0,LOCATION_MZONE)
+	e1:SetValue(2)
+	c:RegisterEffect(e1)
+	--Extra attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(s.atktg)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	--Search or SpSummon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(2,id)
+	e3:SetCondition(s.thcon)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
+end
+function s.atktg(e,c)
+	return c:IsType(TYPE_TUNER)
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return rp==1-tp and re:IsActiveType(TYPE_MONSTER)
+end
+function s.filter(c,e,tp)
+	return c:IsType(TYPE_TUNER) and not c:IsLevel(10) and c:IsFaceupEx()
+		and (c:IsAbleToHand() or c:IsCanBeSpecialSummoned(e,0,tp,false,false))
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and chkc:IsControler(tp) and s.filter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
+	Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE+LOCATION_REMOVED)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE+LOCATION_REMOVED)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToChain() then
+		local b1=tc:IsAbleToHand()
+		local b2=tc:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		local op=aux.SelectFromOptions(tp,
+			{b1,1190},
+			{b2,1152})
+		local res=false
+		if op==1 then
+			res=Duel.SendtoHand(tc,nil,REASON_EFFECT)>0
+			Duel.ConfirmCards(1-tp,tc)
+		else
+			res=Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0
+		end
+		if res then
+			local g=Duel.GetMatchingGroup(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,nil,nil)
+			if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+				local sg=g:Select(tp,1,1,nil)
+				Duel.SynchroSummon(tp,sg:GetFirst(),nil)
+			end
+		end
+	end
+end
