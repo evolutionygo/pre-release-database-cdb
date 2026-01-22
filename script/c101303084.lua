1004b0409bc6fce0d3d390127932732e44bdcb24
YGO-Amiya <a1194601505@gmail.com>
2026-01-23 07:33:54 +0800

    新卡

---
+++ script/c101303084.lua
@@ -0,0 +1,75 @@
+--Mercurium the Living Quicksilver
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	aux.AddXyzProcedure(c,nil,10,2)
+	c:EnableReviveLimit()
+	--material
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.mttg)
+	e1:SetOperation(s.mtop)
+	c:RegisterEffect(e1)
+	--cannot activate
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(0,1)
+	e2:SetValue(s.limval)
+	c:RegisterEffect(e2)
+	--remove material
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.rmtg)
+	e1:SetOperation(s.rmop)
+	c:RegisterEffect(e1)
+end
+function s.mtfilter(c,e)
+	return c:IsType(TYPE_MONSTER)
+		and c:IsCanOverlay() and not (e and c:IsImmuneToEffect(e))
+end
+function s.mttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsType(TYPE_XYZ)
+		and Duel.IsExistingMatchingCard(s.mtfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function s.mtop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+	local g=Duel.SelectMatchingCard(tp,s.mtfilter,tp,LOCATION_DECK,0,1,1,nil,e)
+	if g:GetCount()>0 then
+		Duel.Overlay(c,g)
+	end
+end
+function s.cfilter(c,ec)
+	return c:IsAttribute(ec:GetAttribute())
+end
+function s.limval(e,re,rp)
+	local rc=re:GetHandler()
+	return rc:IsLocation(LOCATION_GRAVE) and re:IsActiveType(TYPE_MONSTER)
+		and Duel.IsExistingMatchingCard(s.cfilter,e:GetHandlerPlayer(),LOCATION_GRAVE,0,1,nil,rc)
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	if e:GetHandler():GetOverlayCount()==0 then
+		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,3000)
+	end
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_EFFECT) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_EFFECT)
+	else
+		Duel.Damage(tp,3000,REASON_EFFECT)
+	end
+end
