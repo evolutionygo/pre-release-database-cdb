29b0d8f28d6df6f88b924fd4bef6b1b4f57e4aa5
purerosefallen <78877@qq.com>
2026-02-06 11:57:37 +0800

    add jest

---
+++ tests/no-redtext.spec.ts
@@ -0,0 +1,41 @@
+import initSqlJs from "sql.js";
+import fs from "node:fs";
+import path from "node:path";
+import { createTest } from "./utility/create-test";
+import { OcgcoreScriptConstants } from "ygopro-msg-encode";
+import { NoEffectAdvancor } from "ygopro-jstest";
+
+describe("No Red Text", () => {
+  it("should not get redtext Lua errors", async () => {
+    const SQL = await initSqlJs();
+    // load everything and make sure no redtext errors occur
+    const files = await fs.promises.readdir(process.cwd());
+    for (const file of files) {
+      if (!file.endsWith(".cdb")) continue;
+      const buf = await fs.promises.readFile(path.resolve(process.cwd(), file));
+      const db = new SQL.Database(buf);
+      const res = db.exec(
+        `SELECT id FROM datas where type != ${OcgcoreScriptConstants.TYPE_MONSTER | OcgcoreScriptConstants.TYPE_NORMAL} and type & ${OcgcoreScriptConstants.TYPE_TOKEN} = 0 and not (alias > 0 and alias - id < 20)`,
+      );
+      for (const data of res) {
+        for (const row of data.values) {
+          const id = row[0] as number;
+          expect(id).toBeGreaterThan(0); // dummy assertion to use id variable
+          console.log(
+            `Testing redtext of card ID: ${id} from database: ${file}`,
+          );
+          await createTest({}, (ygo) =>
+            ygo
+              .addCard({
+                code: id,
+                location: OcgcoreScriptConstants.LOCATION_DECK,
+              })
+              .advance(NoEffectAdvancor()),
+          );
+        }
+      }
+      db.close();
+    }
+  });
+});
+jest.setTimeout(300000);
---
+++ tests/utility/create-test.ts
@@ -0,0 +1,18 @@
+import path from "node:path";
+import { useYGOProTest, YGOProTestOptions } from "ygopro-jstest";
+
+export const createTest = (
+  options: Partial<YGOProTestOptions> = {},
+  cb: Parameters<typeof useYGOProTest>[1],
+) =>
+  useYGOProTest(
+    {
+      ...options,
+      ygoproPath: [
+        ...(options.ygoproPath || []),
+        process.cwd(),
+        path.resolve(process.cwd(), process.env.YGOPRO_PATH || "ygopro"),
+      ],
+    },
+    cb,
+  );
