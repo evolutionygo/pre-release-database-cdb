cd292bb88da79635d343c8013484d257749542ce
Wind2009-Louse <wind.jackey@qq.com>
2026-03-10 23:52:18 +0800

    Fix 三幻魔の失楽園

--- script/c101305048.lua
+++ script/c101305048.lua
@@ -9,7 +9,7 @@ function s.initial_effect(c)
 	--special
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE+CATEGORY_DECKDES)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE+CATEGORY_DECKDES+CATEGORY_GRAVE_SPSUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_FZONE)
 	e2:SetCountLimit(3)
