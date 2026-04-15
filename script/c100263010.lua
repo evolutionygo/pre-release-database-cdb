7b8875d23a6ea31fa7c9ca188b04dcced9110d60
Wind2009-Louse <wind.jackey@qq.com>
2026-04-15 11:39:16 +0800

    Fix D－バースト

--- script/c100263010.lua
+++ script/c100263010.lua
@@ -2,6 +2,7 @@
 local s,id,o=GetID()
 function s.initial_effect(c)
 	aux.AddCodeList(c,17132130)
+	aux.AddSetNameMonsterList(c,0xc008)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
