34e3109ae774bbd988f6d2594bbca10a981877cc
YGO-Amiya <a1194601505@gmail.com>
2026-04-17 21:06:11 +0800

    新卡

---
+++ script/c100261007.lua
@@ -0,0 +1,118 @@
+--心を見通す眼
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_PUBLIC)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCondition(s.picon)
+	e2:SetTargetRange(0,LOCATION_HAND)
+	c:RegisterEffect(e2)
+	--Activate
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_DISABLE)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCountLimit(1,id)
+	e4:SetCondition(s.discon)
+	e4:SetTarget(s.distg)
+	e4:SetOperation(s.disop)
+	c:RegisterEffect(e4)
+end
+function s.cfilter(c)
+	return c:IsFaceupEx() and c:IsSetCard(0x62)
+end
+function s.picon(e)
+	return Duel.IsExistingMatchingCard(s.cfilter,e:GetHandlerPlayer(),LOCATION_ONFIELD+LOCATION_GRAVE,0,1,nil)
+end
+function s.cfilter2(c)
+	return c:IsFaceupEx() and c:IsSetCard(0x62) and c:IsType(TYPE_MONSTER)
+end
+function s.cfilter3(c)
+	return c:IsFaceupEx() and c:IsSetCard(0x62) and c:IsType(TYPE_SPELL)
+end
+function s.discon(e)
+	return Duel.IsExistingMatchingCard(s.cfilter2,e:GetHandlerPlayer(),LOCATION_MZONE+LOCATION_GRAVE,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.cfilter3,e:GetHandlerPlayer(),LOCATION_ONFIELD+LOCATION_GRAVE,0,1,nil)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local ch=Duel.GetCurrentChain()
+	if ch>1 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CODE)
+		local g=Group.CreateGroup()
+		for i=1,ev do
+			local te=Duel.GetChainInfo(i,CHAININFO_TRIGGERING_EFFECT)
+			g:AddCard(te:GetHandler())
+		end
+		local codes={}
+		local ag=Group.CreateGroup()
+		for c in aux.Next(g) do
+			local code=c:GetCode()
+			if not ag:IsExists(Card.IsCode,1,nil,code) then
+				ag:AddCard(c)
+				table.insert(codes,code)
+			end
+		end
+		table.sort(codes)
+		local afilter={codes[1],OPCODE_ISCODE}
+		if #codes>1 then
+			--or ... or c:IsCode(codes[i])
+			for i=2,#codes do
+				table.insert(afilter,codes[i])
+				table.insert(afilter,OPCODE_ISCODE)
+				table.insert(afilter,OPCODE_OR)
+			end
+		end
+		table.insert(afilter,OPCODE_NOT)
+		ag:Clear()
+		local ac=Duel.AnnounceCard(tp,table.unpack(afilter))
+		Duel.SetTargetParam(ac)
+		Duel.SetOperationInfo(0,CATEGORY_ANNOUNCE,nil,0,tp,0)
+	else
+		local ac=Duel.AnnounceCard(tp)
+		Duel.SetTargetParam(ac)
+		Duel.SetOperationInfo(0,CATEGORY_ANNOUNCE,nil,0,tp,0)
+	end
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToChain() and c:IsFaceup() then
+		local ac=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+		e1:SetTarget(s.distg2)
+		e1:SetLabel(ac)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		e1:SetRange(LOCATION_SZONE)
+		c:RegisterEffect(e2)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetCode(EVENT_CHAIN_SOLVING)
+		e2:SetCondition(s.discon2)
+		e2:SetOperation(s.disop2)
+		e2:SetLabel(ac)
+		e2:SetRange(LOCATION_SZONE)
+		e2:SetReset(RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e2)
+	end
+end
+function s.distg2(e,c)
+	return c:IsOriginalCodeRule(e:GetLabel())
+end
+function s.discon2(e,tp,eg,ep,ev,re,r,rp)
+	return re:GetHandler():IsOriginalCodeRule(e:GetLabel())
+end
+function s.disop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateEffect(ev)
+end
