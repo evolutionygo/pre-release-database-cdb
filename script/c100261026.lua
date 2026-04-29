8540031cf5c0715070ef6eabc745ab4e01c618e4
YGO-Amiya <a1194601505@gmail.com>
2026-04-29 20:47:32 +0800

    新卡

---
+++ script/c100261026.lua
@@ -0,0 +1,116 @@
+--ウィッチクラフト・ピューピルズ
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	aux.AddFusionProcFun2(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0x128),s.matfilter,true)
+	--search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_MAIN_END+TIMING_BATTLE_START+TIMING_BATTLE_END)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.cecon)
+	e1:SetCost(s.cecost)
+	e1:SetTarget(s.cetg)
+	e1:SetOperation(s.ceop)
+	c:RegisterEffect(e1)
+	--to grave
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(5206415,1))
+	e3:SetCategory(CATEGORY_TOGRAVE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,id+o)
+	e3:SetCondition(s.tgcon)
+	e3:SetTarget(s.tgtg)
+	e3:SetOperation(s.tgop)
+	c:RegisterEffect(e3)
+end
+function s.matfilter(c)
+	return c:IsRace(RACE_SPELLCASTER)
+end
+function s.cecon(e,tp,eg,ep,ev,re,r,rp)
+	local ph=Duel.GetCurrentPhase()
+	return (ph==PHASE_MAIN1 or (ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE) or ph==PHASE_MAIN2)
+end
+function s.thfilter(c)
+	return c:IsSetCard(0x128) and c:IsType(TYPE_SPELL) and c:IsAbleToGrave()
+end
+function s.cpfilter(c)
+	return c:IsSetCard(0x128) and (c:GetType()==TYPE_SPELL or c:IsType(TYPE_QUICKPLAY))
+		and c:CheckActivateEffect(false,true,false)~=nil
+end
+function s.cecost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local b1=Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil)
+	local b2=Duel.IsExistingMatchingCard(s.cpfilter,tp,LOCATION_HAND,0,1,nil)
+	if chk==0 then return b1 or b2 end
+	local op=aux.SelectFromOptions(tp,
+			{b1,aux.Stringid(id,2),1},
+			{b2,aux.Stringid(id,3),2})
+	e:SetLabel(op)
+	if op==2 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+		local g=Duel.SelectMatchingCard(tp,s.cpfilter,tp,LOCATION_HAND,0,1,1,nil)
+		Duel.ConfirmCards(1-tp,g)
+		e:SetLabelObject(g:GetFirst())
+	end
+end
+function s.cetg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		if not e:IsCostChecked() then
+			e:SetLabel(1)
+		end
+		return e:IsCostChecked() or Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil)
+	end
+	if e:GetLabel()==1 then
+		if e:IsCostChecked() then
+			e:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+		end
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	elseif e:GetLabel()==2 then
+		if e:IsCostChecked() then
+			e:SetCategory(0)
+		end
+		local te,ceg,cep,cev,cre,cr,crp=e:GetLabelObject():CheckActivateEffect(false,true,true)
+		Duel.ClearTargetCard()
+		local tg=te:GetTarget()
+		if tg then tg(e,tp,ceg,cep,cev,cre,cr,crp,1) end
+		te:SetLabelObject(e:GetLabelObject())
+		e:SetLabelObject(te)
+		Duel.ClearOperationInfo(0)
+	end
+end
+function s.ceop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==1 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if g:GetCount()>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	elseif e:GetLabel()==2 then
+		local te=e:GetLabelObject()
+		if not te then return end
+		e:SetLabelObject(te:GetLabelObject())
+		local op=te:GetOperation()
+		if op then op(e,tp,eg,ep,ev,re,r,rp) end
+	end
+end
+function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,LOCATION_REMOVED,LOCATION_REMOVED,1,nil) end
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_REMOVED,LOCATION_REMOVED,1,1,nil)
+	if g:GetCount() then
+		Duel.SendtoGrave(g,REASON_EFFECT+REASON_RETURN)
+	end
+end
