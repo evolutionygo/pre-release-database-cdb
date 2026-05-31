97921d7a7e677489b0d791b42624da892364cf47
Wind2009-Louse <wind.jackey@qq.com>
2026-05-31 23:55:04 +0800

    25.2

--- script/c101306045.lua
+++ script/c101306045.lua
@@ -16,6 +16,7 @@ function s.initial_effect(c)
 	--add counter
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_TOEXTRA+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_BATTLE_DESTROYING)
 	e1:SetCondition(s.ctcon)
@@ -60,7 +61,7 @@ function s.cttg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.ctop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not (c:IsRelateToEffect(e) and c:IsFaceup()) then return end
+	if not (c:IsRelateToChain() and c:IsFaceup()) then return end
 	if not c:AddCounter(COUNTER_CROWN,1) then return end
 	local ct=c:GetCounter(COUNTER_CROWN)
 	if ct==1 then
