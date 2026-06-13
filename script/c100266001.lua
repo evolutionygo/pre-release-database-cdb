d3a8e30133f9dd9292b5d1c253394e82ac80e51b
YGO-Amiya <a1194601505@gmail.com>
2026-06-13 20:37:30 +0800

    修复

--- script/c100266001.lua
+++ script/c100266001.lua
@@ -14,7 +14,7 @@ end
 function s.spfilter(c,e,tp)
 	return c:IsType(TYPE_NORMAL)
 		and (not c:IsLocation(LOCATION_DECK) or c:IsLevelBelow(4))
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function s.spfilter2(c,e,tp)
 	return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
@@ -50,7 +50,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp)
 		if g:GetCount()>0 then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 		end
 	else
 		local op=aux.SelectFromOptions(tp,
