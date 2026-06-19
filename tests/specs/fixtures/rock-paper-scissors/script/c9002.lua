7c20f277d9bf0034ea944553e0cdba6045213c13
purerosefallen <78877@qq.com>
2026-06-20 00:03:19 +0800

    add rps unit tests

---
+++ tests/specs/fixtures/rock-paper-scissors/script/c9002.lua
@@ -0,0 +1,18 @@
+local s,id,o=GetID()
+function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local res=Duel.RockPaperScissors(false)
+	if res==PLAYER_NONE then
+		Duel.Recover(tp,500,REASON_EFFECT)
+	elseif res==tp then
+		Duel.Damage(1-tp,1000,REASON_EFFECT)
+	else
+		Duel.Damage(tp,1000,REASON_EFFECT)
+	end
+end
