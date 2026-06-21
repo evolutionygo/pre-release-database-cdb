38dcb0f93577677f8b35aa683125e8f36d898d32
YGO-Amiya <a1194601505@gmail.com>
2026-06-21 11:41:22 +0800

    修复

--- script/c101306035.lua
+++ script/c101306035.lua
@@ -1,7 +1,7 @@
 --一日万倍龍
 local s,id,o=GetID()
 function s.initial_effect(c)
-	c:EnableCounterPermit(0x76)
+	c:EnableCounterPermit(0x78)
 	--counter
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
@@ -46,41 +46,41 @@ function s.initial_effect(c)
 	c:RegisterEffect(e5)
 end
 function s.ctcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckLPCost(tp,1000) end
-	Duel.PayLPCost(tp,1000)
+	if chk==0 then return Duel.CheckLPCost(tp,100) end
+	Duel.PayLPCost(tp,100)
 end
 function s.cttg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsCanAddCounter(0x76,1) end
+	if chk==0 then return e:GetHandler():IsCanAddCounter(0x78,1) end
 end
 function s.ctop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToChain() and c:IsFaceup() then
-		c:AddCounter(0x76,1)
+		c:AddCounter(0x78,1)
 	end
 end
 function s.cttg2(e,tp,eg,ep,ev,re,r,rp,chk)
 	local lp=math.abs(Duel.GetLP(tp)-Duel.GetLP(1-tp))>0
 	local ct=Math.floor(lp)
-	if chk==0 then return ct>0 and e:GetHandler():IsCanAddCounter(0x76,lp) end
+	if chk==0 then return ct>0 and e:GetHandler():IsCanAddCounter(0x78,lp) end
 end
 function s.ctop2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local lp=math.abs(Duel.GetLP(tp)-Duel.GetLP(1-tp))>0
 	local ct=Math.floor(lp)
 	if c:IsRelateToChain() and c:IsFaceup() and ct>0 then
-		c:AddCounter(0x76,ct)
+		c:AddCounter(0x78,ct)
 	end
 end
 function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetCounter(0x76)>=10
+	return e:GetHandler():GetCounter(0x78)>=10
 end
 function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return c:IsReason(REASON_BATTLE+REASON_EFFECT) and not c:IsReason(REASON_REPLACE)
-		and c:IsCanRemoveCounter(tp,0x76,1,REASON_EFFECT)
+		and c:IsCanRemoveCounter(tp,0x78,1,REASON_EFFECT)
 	end
 	return Duel.SelectEffectYesNo(tp,c,96)
 end
 function s.repop(e,tp,eg,ep,ev,re,r,rp)
-	e:GetHandler():RemoveCounter(tp,0x76,1,REASON_EFFECT)
+	e:GetHandler():RemoveCounter(tp,0x78,1,REASON_EFFECT)
 end
