cee08b624d8bd3b6892d7ad47545d720ef77b01a
YGO-Amiya <a1194601505@gmail.com>
2026-06-24 21:32:57 +0800

    修复

--- script/c100266003.lua
+++ script/c100266003.lua
@@ -90,7 +90,7 @@ function s.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
+	if tc:IsRelateToChain() and aux.NecroValleyFilter()(tc) then
 		Duel.SSet(tp,tc)
 	end
 end
