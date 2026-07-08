8a3e5bdfe95d8c10a79f9e2fc0ca1d279b0dab00
Vee4 <mathew.brooksjr@gmail.com>
2026-07-08 20:20:09 +0000

    Fix Mixousia the Confounder fusion effect

--- script/c101305085.lua
+++ script/c101305085.lua
@@ -70,7 +70,6 @@ end
 function s.filter2(c,e,tp,m,f,gc,chkf)
 	return c:IsType(TYPE_FUSION) and (not f or f(c))
 		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,gc,chkf)
-		and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
 end
 function s.fsptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
